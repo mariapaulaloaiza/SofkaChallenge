e5fa23105df50c9584dd864396d0195eb762ccf4
mariapaulaloaiza <58571961+mariapaulaloaiza@users.noreply.github.com>
2021-07-09 23:44:21 -0500

    Add files via upload

--- juegocarros.sql
+++ juegocarros.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 10-07-2021 a las 06:40:29
+-- Tiempo de generación: 10-07-2021 a las 06:43:36
 -- Versión del servidor: 10.4.11-MariaDB
 -- Versión de PHP: 7.4.6
 
